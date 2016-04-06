3b556a846b809dabba17ce388393f450d32e5254
Sandra Cantero Rabanillo <sdkr@zeta40.aulas.gsyc.es>
2016-04-06 21:12:20 +0200

    Ejercicio resuelto

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+def mostrarContenido(request):
+    paginas = Pages.objects.all()
+    respuesta = "<ul>"
+    for pagina in paginas:
+        respuesta += '<li>' + pagina.name + ": " + pagina.page
+    respuesta += "</ul>"
+    return HttpResponse(respuesta)
+
+def mostrarPagina(request, identificador):
+    pagina = Pages.objects.get(id=int(identificador))
+    respuesta = "<ul>"
+    respuesta += '<li><a href:"' + pagina.name + '">' + pagina.page + '</a>'
+    respuesta += "</ul>"
+    return HttpResponse(respuesta)
+
+@csrf_exempt
+def nuevoContenido(request, name, page):
+    pagina = Pages(name=name, page=page)
+    pagina.save()
+    respuesta = "Nuevo contenido guardado con exito"
+    return HttpResponse(respuesta)
--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'cms.views.mostrarContenido'),
+    url(r'^(\d+)$', 'cms.views.mostrarPagina'),
+    url(r'^nuevocontenido/(.+)\/(.+)$', 'cms.views.nuevoContenido'),
     url(r'^admin/', include(admin.site.urls)),
 )
